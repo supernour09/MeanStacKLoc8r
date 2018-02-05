c25743b4ed70de44ebcb58dd2fdcf2a5e613093b
Nour Ahmed <nourahmedabbas@gmail.com>
2018-02-06 01:27:21 +0200

    get data into controller

--- app_server/controllers/LocationController.js
+++ app_server/controllers/LocationController.js
@@ -1,21 +1,89 @@
 
 
 /* Home page Page  */
-module.exports.listLocations = function(req, res){    
-    res.render('LocationList', { title: 'Loc8er - find place to work' ,
-                                 pageHeader :{
-                                   title : 'loc8r' ,
-                                   strapline : 'Find places to work in :D Work hard '
-                                 } });    
-  };
+module.exports.listLocations = function (req, res) {
+  res.render('LocationList', {
+    title: 'Loc8r - find a place to work with wifi',
+    pageHeader: {
+      title: 'Loc8r',
+      strapline: 'Find places to work with wifi near you!'
+    },
+    locations: [{
+      name: 'Starcups',
+      address: '125 High Street, Reading, RG6 1PS',
+      rating: 3,
+      facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+      distance: '100m'
+    }, {
+      name: 'Cafe Hero',
+      address: '125 High Street, Reading, RG6 2PS',
+      rating: 4,
+      facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+      distance: '200m'
+    }, {
+      name: 'Burger Queen',
+      address: '125 High Street, Reading, RG6 3PS',
+      rating: 2,
+      facilities: ['Food', 'Premium wifi'],
+      distance: '250m'
+    }]
+  }
+  );
+};
 
 
 /* Location Details Page  */
-module.exports.locationDetails = function(req, res){    
-  res.render('Details', { title: 'Location Details' });    
+module.exports.locationDetails = function (req, res) {
+  res.render('Details', {
+    title: 'Starcups',
+    pageHeader: { title: 'Starcups' },
+    sidebar: {
+      context: 'is on Loc8r because it has accessible wifi and space to sit down with your laptop and get some work done.',
+      callToAction: 'If you\'ve been and you like it - or if you don\'t please leave a review to help other people just like you.'
+    },
+    location: {
+    name: 'Starcups',
+    address: '125 High Street, Reading, RG6 1PS',
+    rating: 3,
+    facilities: ['Hot drinks', 'Food', 'Premium wifi'],
+    coords: { lat: 51.455041, lng: -0.9690884 },
+      openingTimes: [{
+        days: 'Monday - Friday',
+        opening: '7:00am',
+        closing: '7:00pm',
+        closed: false
+    }, {
+        days: 'Saturday',
+        opening: '8:00am',
+        closing: '5:00pm',
+        closed: false
+      }, {
+        days: 'Sunday',
+        closed: true
+      }],
+      reviews: [{
+        author: 'Simon Holmes',
+        rating: 5,
+        timestamp: '16 July 2013',
+        reviewText: 'What a great place. I can\'t say enough good things about it.'
+    }, {
+        author: 'Charlie Chaplin',
+        rating: 3,
+        timestamp: '16 June 2013',
+        reviewText: 'It was okay. Coffee wasn\'t great, but the wifi was fast.'
+    }]
+    }
+  });
 };
 
 /* Add Review Page  */
-module.exports.addReview = function(req, res){    
-  res.render('Add Review', { title: 'Add Review' });    
+module.exports.addReview = function (req, res) {
+  res.render('Add Review', { title: 'Add Review' });
+};
+
+
+
+module.exports.homelist = function (req, res) {
+  res.render('locations-list',
+  );
 };
--- app_server/controllers/OtherController.js
+++ app_server/controllers/OtherController.js
@@ -2,5 +2,9 @@
 
 /* for about us page */
 module.exports.about = function(req, res){    
-    res.render('about', { title: 'About' });    
+    res.render('about', {
+       title: 'About',
+       aboutUs: 'Loc8r helps you find places to work when out and about. Perhaps with coffee,cake or a pint? Let Loc8r help you find the place you\'re looking for.'
+      
+      });    
   };
